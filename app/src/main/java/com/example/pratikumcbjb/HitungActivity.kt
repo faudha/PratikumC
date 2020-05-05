10ec763b6521650e301ad6dc7ea6936d2d94666b
faudha <syfudhfaudhansm@gmail.com>
2020-05-05 12:14:59 +0700

    tes perkalian Hitung Hasil

--- app/src/main/java/com/example/pratikumcbjb/HitungActivity.kt
+++ app/src/main/java/com/example/pratikumcbjb/HitungActivity.kt
@@ -2,11 +2,22 @@ package com.example.pratikumcbjb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hitung.*
 
 class HitungActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hitung)
+
+        buttonHitung.setOnClickListener {
+            val panjang=editPanjang.text.toString().trim().toFloat()
+            val lebar=editLebar.text.toString().trim().toFloat()
+            val tinggi=editTinggi.text.toString().trim().toFloat()
+
+            textHasil.text=(panjang * lebar * tinggi).toString()
+
+
+        }
     }
 }
